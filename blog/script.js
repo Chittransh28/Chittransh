3d6ed79afce956b447aaeefefe8feddef0969a01
Chittransh28 <chittranshprajjwal@gmail.com>
2025-07-30 00:47:11 +0530

    Update script.js

--- blog/script.js
+++ blog/script.js
@@ -1,6 +1,84 @@
 document.addEventListener('DOMContentLoaded', () => {
     // --- Particle.js Background ---
-    particlesJS('particles-js-blog', { /* ... Paste your particles.js config from previous files here ... */ });
+    particlesJS('particles-js-blog', {  
+        "particles": {
+            "number": {
+                "value": 60, // Number of particles
+                "density": {
+                    "enable": true,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": "#8b949e" // Particle color
+            },
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 0,
+                    "color": "#000000"
+                }
+            },
+            "opacity": {
+                "value": 0.4,
+                "random": true,
+                "anim": {
+                    "enable": true,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 3,
+                "random": true,
+                "anim": {
+                    "enable": false
+                }
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 150,
+                "color": "#30363d", // Line color
+                "opacity": 0.4,
+                "width": 1
+            },
+            "move": {
+                "enable": true,
+                "speed": 2,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "grab" // 'grab' or 'repulse'
+                },
+                "onclick": {
+                    "enable": false,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 140,
+                    "line_opacity": 1
+                },
+                "repulse": {
+                    "distance": 200,
+                    "duration": 0.4
+                }
+            }
+        },
+        "retina_detect": true
+    });
 
     const searchInput = document.getElementById('search-input');
     const postGrid = document.getElementById('post-grid');
